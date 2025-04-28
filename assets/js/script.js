489ed279c61cfc4b5e042a620e7f4ec0c09b7fc3
ohhMyVenuss <minhhbn.24it@vku.udn.vn>
2025-04-29 01:11:58 +0700

    update main menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,43 @@ function scrollCarousel(button, direction) {
 function goToTransport() {
   window.location.href = 'transport.html';
 }
+document.addEventListener('DOMContentLoaded', () => {
+  const slidesContainer = document.querySelector('.slides');
+  const slides = document.querySelectorAll('.slide');
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  let currentIndex = 0;
+
+  function initSlider() {
+      slides.forEach((slide, index) => {
+          if (index === 0) slide.classList.add('active');
+          else slide.classList.remove('active');
+      });
+  }
+
+  function updateSlider() {
+      slidesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+      
+      slides.forEach((slide, index) => {
+          slide.classList.toggle('active', index === currentIndex);
+      });
+  }
+
+  nextBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % slides.length;
+      updateSlider();
+  });
+
+  prevBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      updateSlider();
+  });
+
+  setInterval(() => {
+      currentIndex = (currentIndex + 1) % slides.length;
+      updateSlider();
+  }, 8000);
+
+
+  initSlider();
+});
